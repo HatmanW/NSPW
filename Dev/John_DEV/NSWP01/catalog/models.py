8c2183646ad0f954684f86808b6742b66fe5901c
HatmanW <118311932+HatmanW@users.noreply.github.com>
2023-03-28 20:05:27 -0500

    I have no idea
    randompush

--- Dev/John_DEV/NSWP01/catalog/models.py
+++ Dev/John_DEV/NSWP01/catalog/models.py
@@ -1,3 +1,31 @@
-from django.db import models
 
+from django.db import models
+from django.urls import reverse  # Used to generate URLs by reversing the URL patterns
+import uuid  # Required for unique shoe instances
+
+from datetime import date
 # Create your models here.
+
+
+#models for COLOR, BRAND, SIZE, and TYPE
+# class color(models.Model):
+#     '''model representing a shoe color'''
+#     name = models.CharField(max_length=200, help_text='Enter a Shoe Color (Red, Black, White, Tan)')
+#
+#     def __str__(self):
+#         '''string for representing the Model Object.'''
+#         return self.name
+# class Brand(models.Model):
+#     """Model representing shoe brand."""
+#     name = models.CharField(max_length=200, help_text='Enter a book genre (e.g. Nike, Addidas, Converse, Etc.)')
+#
+#     def get_absolute_url(self):
+#         """Returns the URL to access a particular brand instance."""
+#
+#         return reverse('brand_detail', args=[str(self.id)])
+#     def __str__(self):
+#     """String for representing the Model object."""
+#         return f''
+#
+# class Size(models.Model):
+#     '''Model representing shoe size.'''
