dabff50e2f10ae312bfedf5dab1284742c628400
myhpnguyen <116761525+myhpnguyen@users.noreply.github.com>
2023-04-18 14:34:29 -0500

    7th push
    has password reset htmls

--- Dev/My_DEV/NSWP01/shop/urls.py
+++ Dev/My_DEV/NSWP01/shop/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
-from django.contrib import admin
 from . import views
+from django.contrib.auth import views as auth_views
+
 
 app_name = 'shop'
 
@@ -9,5 +10,9 @@ urlpatterns = [
     path('product_list/', views.product_list, name='product_list'),
     path('<slug:category_slug>/', views.product_list, name='product_list_by_category'),
     path('<int:id>/<slug:slug>/', views.product_detail, name='product_detail'),
+    path('password_reset/', auth_views.PasswordResetView.as_view(), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
